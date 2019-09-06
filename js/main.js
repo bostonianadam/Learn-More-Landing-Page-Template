6f6f9cb2cf03c8c7a65317aceb70dd7dfa278981
BostonianAdam <bostonianadam@gmail.com>
2019-09-06 13:04:19 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-
+window.onscroll = function() {
+    scrollFunction()
+};
+function scrollFunction() {
+    let navbar = document.getElementById("navbar");
+    let logo = document.getElementById("logo");
+    if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
+        navbar.classList.add("navbarScroll");
+        navbar.classList.add("shadow");
+        logo.classList.add("logoScroll");
+    } else {
+        navbar.classList.remove("navbarScroll");
+        navbar.classList.remove("shadow");
+        logo.classList.remove("logoScroll");
+    }
+}
